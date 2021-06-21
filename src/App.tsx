fcf547aa11622eadffe7851e65cd3790d4b15d7f
Ada Rosa <adrosa@microsoft.com>
2021-06-21 02:48:05 +0000

    i don't like react very much

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from "react";
+import "./index.css"
 
-function App() {
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Search from "./Search";
+import About from "./About";
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <Router>
+    <main>
+      <nav>
+        <ul>
+          <li><a href="/">Home</a></li>
+          <li><a href="/search">Search</a></li>
+          <li><a href="/about">About</a></li>
+        </ul>
+      </nav>
+      <Route path="/" render={() => <h1>Welcome!</h1>} />
+      {/* <Route path="/search" component={Search} /> */}
+      <Route path="/about" render={About} />
+
+    </main>
+</Router>
   );
 }
-
-export default App;
